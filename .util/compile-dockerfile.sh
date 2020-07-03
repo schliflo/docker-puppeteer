f855b01b77e56bf9c0ca8ac1c204bc06a23d502c
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-07-03 18:08:52 +0200

    fix chrome revision parsing

--- .util/compile-dockerfile.sh
+++ .util/compile-dockerfile.sh
@@ -2,6 +2,6 @@
 
 yarn --frozen-lockfile
 PPTR_VERSION=$(node -e "console.log(require('./node_modules/puppeteer/package.json')['version'])")
-CHROME_REVISION=$(node -e "console.log(require('./node_modules/puppeteer/package.json')['puppeteer']['chromium_revision'])")
+CHROME_REVISION=$(node .util/revision.js chromium)
 
 sed -e "s/<PPTR_VERSION>/$PPTR_VERSION/" -e "s/<CHROME_REVISION>/$CHROME_REVISION/" Dockerfile.template > Dockerfile
