271efddf3e4deca323c3c3cc774aee28432ea4bf
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2022-10-31 15:37:11 +0100

    switch to google provided base image (#103)
    * switch to google provided docker image as base
    
    * add more env variables

--- .util/compile-dockerfile.sh
+++ .util/compile-dockerfile.sh
@@ -2,6 +2,5 @@
 
 yarn --frozen-lockfile
 PPTR_VERSION=$(node -e "console.log(require('./node_modules/puppeteer/package.json')['version'])")
-CHROME_REVISION=$(node .util/revision.js chromium)
 
-sed -e "s/<PPTR_VERSION>/$PPTR_VERSION/" -e "s/<CHROME_REVISION>/$CHROME_REVISION/" Dockerfile.template > Dockerfile
+sed -e "s/<PPTR_VERSION>/$PPTR_VERSION/" Dockerfile.template > Dockerfile
--- .util/revision.js
+++
@@ -1,4 +0,0 @@
-const revisions = require('puppeteer/lib/cjs/puppeteer/revisions.js');
-const args = process.argv.slice(2);
-
-console.log(revisions['PUPPETEER_REVISIONS'][args[0]]);
